665962535fef5bfccafacf116d2ab6a8a31c0da5
gitavenian <gitaravenian@gmail.com>
2025-01-09 12:52:56 +0300

    test1

--- server/routes/home.js
+++ server/routes/home.js
@@ -7,7 +7,7 @@ import { updateServices} from '../controllers/servicesController.js';
 const router = express.Router();
 
 router.get('/', getAbout);
-router.post('/about/update', upload.single('image'), updateAbout);
+router.post('/about/update', upload.single('photo'), updateAbout);
 router.post('/features/update', updateFeatures);
 router.post('/services/update', updateServices);
 
